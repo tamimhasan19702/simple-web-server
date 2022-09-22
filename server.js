87a60eb4bcba3fd473f1b34522f50ec9e5fd649c
tamimhasan19702 <tamimhasancu@gmail.com>
2022-09-22 20:28:56 +0600

    created the basic functionality

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-const http = require("http") // requiring "http" library from the nodejs
+const http = require("http") // to use a library in nodejs we need to use "require" method and created a variable named http
+                            // using "http" library in nodejs using require method
+
+const port = 1000;  // using this port variable to declare a port for our server;                            
+
+
+//using the "http" library to create a server
+
+const server = http.createServer((req,res) => { //used arrow function in the createServer method in http library
+
+})
+
+
+// setup of server
+
+server.listen(port, function(error){   //server starts listening at the declared port number and if there's error it will use that error function.
+     if(error){
+        console.log(`Error happened in this Server. Error : ${error}`)
+     } else{
+        console.log(`Server is listening to the port. port : ${port}`)
+     }
+
+})
