524c56d8555189d127ed069bc358b6469ad977d1
tamimhasan19702 <tamimhasancu@gmail.com>
2022-09-22 23:06:21 +0600

    final changes

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const http = require("http") // to use a library in nodejs we need to use "require" method and created a variable named http
-                            // using "http" library in nodejs using require method
-
+                           // using "http" library in nodejs using require method
+const fs = require('fs') // using the fs module to read file directory in my server
 const port = 1000;  // using this port variable to declare a port for our server;                            
-
-
 //using the "http" library to create a server
-
-const server = http.createServer((req,res) => { //used arrow function in the createServer method in http library
-  
-    res.write('Hello there welcome to my server'); //creating custom response
-    res.end()
-
+const server = http.createServer((req,res) => { //used arrow function in the createServer method in http library   
+    res.writeHead(200, {'Content-type': 'text/html'})
+    fs.readdir('/directory',function(error,data){
+        if(error){
+            res.writeHead(404)
+            res.write(`Error: there is no file available. ${error}`)
+        }else{
+           res.write(data) 
+        }
+        res.end()
+    })
 })
-
-
 // setup of server
-
 server.listen(port, function(error){   //server starts listening at the declared port number and if there's error it will use that error function.
      if(error){
         console.log(`Error happened in this Server. Error : ${error}`)
